265942264ecf8dfd5656ec54fd64240c0e955b6f
Duamelo <david.dosseh@imsp-uac.org>
2022-06-06 19:24:00 +0200

    code clean

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $(document).ready(function(){
+      $('.tabs').tabs();
+    });
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+    });
+      
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
